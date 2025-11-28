4ab364b47bad39e79433d82f61ed8f0721dfacfb
SuChang <1614754563@qq.com>
2025-11-29 00:14:23 +0800

    优化逻辑

--- ReadCADService.cs
+++ ReadCADService.cs
@@ -14,19 +14,28 @@ namespace Su.AutoCAD2Revit
     public class ReadCADService : IDisposable
     {
         //图纸element
-        private ImportInstance importInstance;
-
         private string cacheDwgFile;//缓存图纸的路径
-        private Transform importInstanceTransform;//图纸的transform
+        private Transform importInstanceTransform = Transform.Identity;//图纸的transform
         private double levelHeight = 0;//图纸的绝对标高z
+        private Services service;
+        private Database database;
+        private BlockTable table;
+        private BlockTableRecord record;
+        private readonly FileOpenMode fileOpenMode;
+        private readonly string blockTableRecord;
+        private readonly bool allowCPConversion;
+        private readonly string password;
 
         /// <summary>
         /// 构造函数
         /// </summary>
-        /// <param name="importInstance">导入实例</param>
-        public ReadCADService(ImportInstance importInstance)
+        /// <param name="importInstance">链接的AutoCAD图纸</param>
+        /// <param name="fileOpenMode"></param>
+        /// <param name="blockTableRecord"></param>
+        /// <param name="allowCPConversion"></param>
+        /// <param name="password"></param>
+        public ReadCADService(ImportInstance importInstance, FileOpenMode fileOpenMode = FileOpenMode.OpenForReadAndWriteNoShare, string blockTableRecord = "*MODEL_SPACE", bool allowCPConversion = true, string password = "") : this(fileOpenMode, blockTableRecord, allowCPConversion, password)
         {
-            this.importInstance = importInstance;
             cacheDwgFile = Directory.GetParent(GetType().Assembly.Location).FullName + $"\\{Path.GetFileName(importInstance.GetCADPath())}";
             //Doc.GetCADPath(dwgElement).SmartCopyFile(cacheDwgFile);
             importInstanceTransform = importInstance.GetTransform();
@@ -39,10 +48,64 @@ namespace Su.AutoCAD2Revit
             {
                 levelHeight = importanLevel.ProjectElevation + importInstance.get_Parameter(BuiltInParameter.IMPORT_BASE_LEVEL_OFFSET).AsDouble();
             }
+            Init();
+        }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="dwgfile">DWG文件绝对路径（包含文件拓展名）</param>
+        /// <param name="levelHeightZ">图纸所在的绝对标高z</param>
+        /// <param name="placement">图纸对齐方式</param>
+        /// <param name="fileOpenMode"></param>
+        /// <param name="blockTableRecord"></param>
+        /// <param name="allowCPConversion"></param>
+        /// <param name="password"></param>
+        public ReadCADService(string dwgfile, double levelHeightZ, ImportPlacement placement = ImportPlacement.Origin, FileOpenMode fileOpenMode = FileOpenMode.OpenForReadAndWriteNoShare, string blockTableRecord = "*MODEL_SPACE", bool allowCPConversion = true, string password = "") : this(fileOpenMode, blockTableRecord, allowCPConversion, password)
+        {
+            cacheDwgFile = Directory.GetParent(GetType().Assembly.Location).FullName + $"\\{Path.GetFileName(dwgfile)}";
+            levelHeight = levelHeightZ;
+            Init();
+        }
+
+        private ReadCADService(FileOpenMode fileOpenMode, string blockTableRecord, bool allowCPConversion, string password)
+        {
+            this.fileOpenMode = fileOpenMode;
+            this.blockTableRecord = blockTableRecord;
+            this.allowCPConversion = allowCPConversion;
+            this.password = password;
         }
 
         private ReadCADService()
         {
+
+        }
+
+        private void Init()
+        {
+            this.service = new Services();
+            this.database = new Database(false, false);
+            try
+            {
+                database.ReadDwgFile(cacheDwgFile, fileOpenMode, allowCPConversion, password);
+            }
+            catch
+            {
+                throw new Exception($"图纸读取失败！可能是您的图纸版本高于AutoCAD2013，或您的图纸进行了特殊加密，或您链接的图纸失去链接！请检查后重试");
+            }
+            try
+            {
+                database.ExportBlocks(blockTableRecord);
+            }
+            catch (Exception ex)
+            {
+            }
+            this.table = (BlockTable)database.BlockTableId.GetObject(OpenMode.ForWrite);
+            this.record = (BlockTableRecord)table[blockTableRecord].GetObject(OpenMode.ForWrite);
+        }
+
+        private ReadCADService()
+        {
+
         }
 
         private void DeleteCacheFile()
@@ -53,66 +116,40 @@ namespace Su.AutoCAD2Revit
             }
             catch
             {
+
             }
         }
 
         /// <summary>
         /// 取得该图纸中的所有文字信息
         /// </summary>
-        /// <param name="fileOpenMode"></param>
-        /// <param name="blockTableRecord"></param>
-        /// <param name="allowCPConversion"></param>
-        /// <param name="password"></param>
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
-        public List<CADTextModel> GetAllTexts(FileOpenMode fileOpenMode = FileOpenMode.OpenForReadAndWriteNoShare, string blockTableRecord = "*MODEL_SPACE", bool allowCPConversion = true, string password = "")
+        public List<CADTextModel> GetAllTexts()
         {
             List<CADTextModel> listCADModels = [];
-            using (new Services())
+            foreach (ObjectId id in record)
             {
-                using Database database = new(false, false);
-                try
+                using Entity entity = (Entity)id.GetObject(OpenMode.ForRead, false, false);
+                switch (entity.GetType().Name)
                 {
-                    database.ReadDwgFile(cacheDwgFile, fileOpenMode, allowCPConversion, password);
-                }
-                catch
-                {
-                    throw new Exception($"图纸读取失败！可能是您的图纸版本高于AutoCAD2013，或您的图纸进行了特殊加密，或您链接的图纸失去链接！请检查后重试");
-                }
-                try
-                {
-                    database.ExportBlocks(blockTableRecord);
-                }
-                catch (Exception ex)
-                {
-                }
-                using var trans = database.TransactionManager.StartTransaction();
-                using BlockTable table = (BlockTable)database.BlockTableId.GetObject(OpenMode.ForWrite);
-                using BlockTableRecord record = (BlockTableRecord)table[blockTableRecord].GetObject(OpenMode.ForWrite);
-                foreach (ObjectId id in record)
-                {
-                    using Entity entity = (Entity)id.GetObject(OpenMode.ForRead, false, false);
-                    switch (entity.GetType().Name)
-                    {
-                        case nameof(DBText):
-                            DBText text = (DBText)entity;
-                            var dbLocation = text.Position.ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
-                            var dbCenter = text.GeometricExtents.Center().ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
-                            CADTextModel model = new CADTextModel(dbLocation, dbCenter, text.TextString, text.Layer, text.Rotation, importInstanceTransform, text.BlockName);
-                            listCADModels.Add(model);
-                            break;
+                    case nameof(DBText):
+                        DBText text = (DBText)entity;
+                        var dbLocation = text.Position.ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
+                        var dbCenter = text.GeometricExtents.Center().ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
+                        CADTextModel model = new(dbLocation, dbCenter, text.TextString, text.Layer, text.Rotation, importInstanceTransform, text.BlockName);
+                        listCADModels.Add(model);
+                        break;
 
-                        case nameof(MText):
-                            MText mText = (MText)entity;
-                            var mtLocation = mText.Location.ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
-                            var mtCenter = mText.GeometricExtents.Center().ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
-                            CADTextModel acDbMTextModel = new CADTextModel(mtLocation, mtCenter, mText.Text, mText.Layer, mText.Rotation, importInstanceTransform, mText.BlockName);
-                            listCADModels.Add(acDbMTextModel);
-                            break;
-                    }
+                    case nameof(MText):
+                        MText mText = (MText)entity;
+                        var mtLocation = mText.Location.ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
+                        var mtCenter = mText.GeometricExtents.Center().ToRevitPoint().Transform(importInstanceTransform).SetZ(levelHeight);
+                        CADTextModel acDbMTextModel = new(mtLocation, mtCenter, mText.Text, mText.Layer, mText.Rotation, importInstanceTransform, mText.BlockName);
+                        listCADModels.Add(acDbMTextModel);
+                        break;
                 }
             }
-
             return listCADModels;
         }
 
@@ -483,6 +520,17 @@ namespace Su.AutoCAD2Revit
 
         public void Dispose()
         {
+            try
+            {
+                this.record.Dispose();
+                this.table.Dispose();
+                this.database.Dispose();
+                this.service.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
             DeleteCacheFile();
         }
     }
